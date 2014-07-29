c482205398574b608a45b2b28244e2072d36c84f
Dan Newcome <dnuke@yahoo-inc.com>
2014-07-29 07:00:27 -0700

    setting clock speed lower

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
 var SPI = require('pi-spi');
 
 var spi = SPI.initialize("/dev/spidev0.0"),
-    test = Buffer("Hello, World!");
+    test = Buffer("Hello, this is flickrframe!!!!");
 
-console.log(spi.clockSpeed());
+// set to 1MHz, data loss occurs at 4MHz
+spi.clockSpeed(1000000);
 
 // reads and writes simultaneously
 spi.transfer(test, test.length, function (e,d) {
@@ -13,7 +14,6 @@ spi.transfer(test, test.length, function (e,d) {
     if (test.toString() === d.toString()) {
         console.log('read correctly');
     } else {
-        // NOTE: this will likely happen unless MISO is jumpered to MOSI
         console.warn('failed');
         process.exit(-2);
     }
